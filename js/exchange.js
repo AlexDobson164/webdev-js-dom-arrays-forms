8a2e60c32a72094307bd1bbb3126a011f1896789
AlexDobson164 <alexdobson04@gmail.com>
2022-11-18 09:24:25 +0000

    added exchange rate calculator

--- js/exchange.js
+++ js/exchange.js
@@ -1,5 +1,23 @@
+let pricePounds;
+let exchangeRate = 1.15;
+let priceEuros;
+
+let convertForm = document.getElementById('myForm');
+let msg = document.getElementById('msg');
+
 convertForm.addEventListener('submit', function(ev){
     ev.preventDefault();
-    let poundAmount = document.getElementById('pounds').value;
-    console.info(poundAmount);
+    pricePounds = parseFloat(document.getElementById('pounds').value);
+    if(isNaN(pricePounds)){
+        msg.style.display = "block";
+        msg.innerHTML = "You must enter a number";
+        msg.setAttribute('class', 'error');
+    }
+    else{
+        priceEuros = pricePounds * exchangeRate;
+        msg.style.display = "block";
+    priceEuros = priceEuros.toFixed(2);
+    msg.innerHTML = "You will get &euro;"+priceEuros;
+    msg.removeAttribute('class');
+    }
 })
